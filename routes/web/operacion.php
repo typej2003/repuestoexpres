36e8ecdc91468b48b195514880fb1d18c29ccca4
Typej2003 <typej2003@gmail.com>
2024-10-30 12:30:59 -0400

    Actualizacion lista

--- routes/web/operacion.php
+++ routes/web/operacion.php
@@ -7,7 +7,8 @@ use App\Http\Livewire\Operacion\MakePayment;
 use App\Http\Livewire\Operacion\Pasarela;
 
 
-use App\Http\Livewire\components\Currency;
+use App\Http\Livewire\Components\Currency;
+
 Route::get('/showCurrency', Currency::class,)->name('showCurrency');
 
 Route::get('/MakePayment/{comercioId}', MakePayment::class)->name('MakePayment')->middleware('auth');
